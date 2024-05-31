83a034ac9a1b4cb4e891dde7db72abd23d779c63
nlclover <timcooktrue@yandex.ru>
2024-05-31 12:19:35 +0300

    new file:   .gitmodules 	modified:   main.py 	modified:   model.py 	new file:   proto 	deleted:    service.py

--- main.py
+++ main.py
@@ -1,8 +1,127 @@
+import grpc
+import templates_pb2
+import templates_pb2_grpc
+from concurrent import futures
+import model
+
+
+grpc_port = '[::]:50051'
+
+class TemplatesServicer(templates_pb2_grpc.TemplatesServicer):
+    
+    def CreateTemplate(self, request, context):
+        try:
+            id = model.AddTemplate(request.name, request.description)
+            return templates_pb2.IdStruct(id=id)
+        except Exception as e:
+            print("Error in CreateTemplate:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+            return templates_pb2.IdStruct()
+            
+
+    def CreateLink(self, request, context):
+        try:
+            id = model.AddFeatureTemplateLink(request.feature_id, request.template_id)
+            return templates_pb2.IdStruct(id=id)
+        except Exception as e:
+            print("Error in CreateLink:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+            return templates_pb2.IdStruct()
+
+
+
+    def GetAllTemplates(self, request, context):
+        try:
+            ret_templates = templates_pb2.TemplatesList()
+            templates_result = model.GetAllTemplates()
+
+            for template in templates_result:
+                temp_struct = templates_pb2.TemplateStruct(id=template["id"], name=template["name"], description=template["description"])
+                ret_templates.items.append(temp_struct)
+
+            return ret_templates
+        except Exception as e:
+            print("Error in GetAllTemplates:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+            return templates_pb2.TemplatesList()
+            
+
+    
+    def GetFeaturesByTemplateId(self, request, context):
+        try:
+            ret_features = templates_pb2.FeaturesList()
+            templates_result = model.GetFeaturesByTemplateId(request.id)
+            print(templates_result)
+
+            for feature in templates_result:
+                print(feature)
+
+                temp_struct = templates_pb2.FeatureStruct(id=feature["id"], name=feature["name"], priority_id=feature["priority_id"])
+                print("add")
+                ret_features.items.append(temp_struct)
+
+            return ret_features
+        except Exception as e:
+            print("Error in GetFeaturesByTemplateId:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+            return templates_pb2.FeaturesList()
+        
 
 
 
 
 
 
-if __name__ == "main":
-    main()
+
+
+
+    def DeleteLink(self, request, context):
+        try:
+            print(request.feature_id)
+            model.DeleteFeatureTemplateLink(request.feature_id, request.template_id)
+        except Exception as e:
+            print("Error in DeleteLink:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+        
+        return templates_pb2.Empty()
+
+
+
+    def DeleteTemplate(self, request, context):
+        try:
+            model.DeleteTemplate(request.id)
+        except Exception as e:
+            print("Error in DeleteTemplate:", e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(str(e))
+        
+        return templates_pb2.Empty()
+
+
+
+
+
+
+
+
+
+
+
+def serve():
+    try:
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        templates_pb2_grpc.add_TemplatesServicer_to_server(TemplatesServicer(), server)
+        server.add_insecure_port(grpc_port)
+        server.start()
+        server.wait_for_termination()
+    except Exception as e:
+        print("Error in server:", e)
+
+if __name__ == "__main__":
+    print(f"Run server on {grpc_port}")
+    serve()
--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 from config import *
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.exc import IntegrityError
+
 
 Base = declarative_base()
 engine = create_engine(psql_conn_url)
@@ -23,6 +26,7 @@ class Feature(Base):
     # Отношение многие-ко-многим с таблицей Template через таблицу FeaturesTemplates
     templates = relationship("FeaturesTemplates", back_populates="feature")
 
+
 class Template(Base):
     """
     Модель таблицы templates.
@@ -37,6 +41,7 @@ class Template(Base):
     # Отношение многие-ко-многим с таблицей Feature через таблицу FeaturesTemplates
     features = relationship("FeaturesTemplates", back_populates="template")
 
+
 class FeaturesTemplates(Base):
     """
     Модель таблицы features_templates.
@@ -45,13 +50,14 @@ class FeaturesTemplates(Base):
     __tablename__ = 'features_templates'
 
     id = Column(Integer, primary_key=True, autoincrement=True) 
-    id_feature = Column(Integer, ForeignKey('features.id'), nullable=False)
-    id_template = Column(Integer, ForeignKey('templates.id'), nullable=False) 
+    feature_id = Column(Integer, ForeignKey('features.id'), nullable=False)
+    template_id = Column(Integer, ForeignKey('templates.id'), nullable=False) 
 
     # Отношение многие-к-одному с таблицей Feature
     feature = relationship("Feature", back_populates="templates")
     # Отношение многие-к-одному с таблицей Template
     template = relationship("Template", back_populates="features")
+
 
 class Priority(Base):
     """
@@ -67,9 +73,173 @@ class Priority(Base):
     feature = relationship("Feature", back_populates="priorities")
 
 
-
 def CreateTables():
     Base.metadata.create_all(engine)
 
+def GetSession():
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 CreateTables()
+
+
+
+class ModelException(Exception):
+    pass
+
+
+
+
+
+def AddTemplate(name, description=None):
+    """
+    Добавляет новый шаблон в базу данных и возвращает его id.
+    """
+    with GetSession() as session:
+        try:
+            template = Template(name=name, description=description)
+            session.add(template)
+            session.commit()
+            return template.id
+        except IntegrityError:
+            session.rollback()
+            return None
+        
+
+def AddFeatureTemplateLink(feature_id, template_id):
+    """
+    Добавляет связь между функциональностью и шаблоном в базу данных и возвращает id новой записи.
+    """
+    with GetSession() as session:
+        res = session.query(FeaturesTemplates).filter(FeaturesTemplates.id == template_id, FeaturesTemplates.feature_id == feature_id).first()
+        if res:
+            print(f"Запись уже имеется, скипаем feature_id: {feature_id}, template_id: {template_id}")
+            return
+
+        try:
+            feature_template = FeaturesTemplates(feature_id=feature_id, template_id=template_id)
+            session.add(feature_template)
+            session.commit()
+            return feature_template.id
+        except IntegrityError:
+            session.rollback()
+            return None
+        
+
+
+
+
+
+
+def UpdateTemplateFeaturesLink(link_id, template_id, feature_id):
+    """
+    Обновление связи между шаблоном и фичей
+    """
+    with GetSession() as session:
+        res = session.query(Template).filter(Template.id == template_id).first()
+        if res:
+            res.template_id = template_id
+            res.feature_id = feature_id
+            session.commit()
+        else:
+            raise ModelException("Не найдена структура")
+
+
+def UpdateTemplate(template_id, name, description):
+    """
+    Обновление шаблона
+    """
+    with GetSession() as session:
+        res = session.query(Template).filter(Template.id == template_id).first()
+        if res:
+            res.name = name
+            res.description = description
+            session.commit()
+        else:
+            raise ModelException("Не найдена структура")
+
+
+
+
+
+
+
+def GetAllTemplates():
+    """
+    Получение всех шаблонов
+    """
+    with GetSession() as session:
+        templates = []
+        templates_result = session.query(Template).all()
+        for template in templates_result:
+            template_dict = {
+                "id": template.id,
+                "name": template.name,
+                "description": template.description
+            }
+            templates.append(template_dict)
+        return templates
+
+
+
+
+
+
+def GetFeaturesByTemplateId(template_id):
+    """
+    Получение всех функциональностей по идентификатору шаблона.
+    """
+    features_dicts = []
+    with GetSession() as session:
+        
+        links = session.query(FeaturesTemplates).filter(FeaturesTemplates.template_id == template_id).all()
+        
+        for link in links:
+            feature = session.query(Feature).filter(Feature.id == link.feature_id).first()
+            if feature:
+                feature_dict = {
+                    "id": feature.id,
+                    "name": feature.name,
+                    "priority_id": feature.priority_id
+                }
+                features_dicts.append(feature_dict)
+        return features_dicts
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+def DeleteFeatureTemplateLink(feature_id, template_id):
+    """
+    Удаляет связь меду функциональностью и шаблоном в базе данных
+    """
+    with GetSession() as session:
+        res = session.query(FeaturesTemplates).filter(FeaturesTemplates.template_id == template_id, FeaturesTemplates.feature_id == feature_id).first()
+        if res:
+            session.delete(res)
+            session.commit()
+        else:
+            raise ModelException("Не найдена структура")
+
+
+
+def DeleteTemplate(template_id):
+    """
+    Удаляет шаблон
+    """
+    with GetSession() as session:
+        res = session.query(Template).filter(Template.id == template_id).first()
+        if res:
+            session.delete(res)
+            session.commit()
+        else:
+            raise ModelException("Не найдена структура")
